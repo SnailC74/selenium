79fae39fb3a80b4507e19c583d225210ceec6f27
Hongyu Chen <60499437+SnailC74@users.noreply.github.com>
2022-10-15 14:34:28 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,6 @@
+import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-# 打开浏览器
-driver = webdriver.chrome()
-# 加载网页
-driver.get("https://www.baidu.com")
 
 # ID定位
 driver.find.element(By.ID, "kw").send_keys("University of Regina")
@@ -15,3 +11,17 @@ driver.find.element(By.LINK_TEXT, "地图").click()
 # Partial_link定位
 driver.find.element(By.PARTIAL_LINK, "图").click()
 
+class TestCase(unittest.TestCase):
+  
+    def test_1_login(self):
+        global driver
+        # 打开浏览器
+        driver = webdriver.chrome()
+        # 加载网页
+        driver.get("https://https://www.uregina.ca/")
+        # Link_text定位
+        driver.find.element(By.LINK_TEXT, "Current Students").click()
+        # Link_text定位
+        driver.find.element(By.LINK_TEXT, "UR Courses").click()
+        
+        
