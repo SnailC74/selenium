c2adf7609b9f1b43d112ff4bc653d87a36eb4bf8
Hongyu Chen <60499437+SnailC74@users.noreply.github.com>
2022-10-16 14:33:01 +0800

    Update test.py

--- test.py
+++ test.py
@@ -22,9 +22,9 @@ class TestCase(unittest.TestCase):
         driver = webdriver.chrome()
         # 加载网页
         driver.get("https://https://www.uregina.ca/")
-        # Link_text定位
+        # 选择当前学生界面
         driver.find.element(By.LINK_TEXT, "Current Students").click()
-        # Link_text定位
+        # 选择自助服务界面
         driver.find.element(By.LINK_TEXT, "UR Self-Service").click()
         # 输入用户ID和密码
         driver.find.element(By.NAME, "sid").send_keys("*********")
@@ -43,9 +43,9 @@ class TestCase(unittest.TestCase):
         driver.implicitly_wait(10)
         # 加载网页
         driver.get("https://https://www.uregina.ca/")
-        # Link_text定位
+        # 选择当前学生界面
         driver.find.element(By.LINK_TEXT, "Current Students").click()
-        # Link_text定位
+        # 选择自助服务界面
         driver.find.element(By.LINK_TEXT, "UR Self-Service").click()
         # 输入用户ID和密码
         driver.find.element(By.NAME, "sid").send_keys("*********")
@@ -59,21 +59,21 @@ class TestCase(unittest.TestCase):
         driver.find.element(By.LINK_TEXT, "Registration").click()
         # 查询课程
         driver.find.element(By.LINK_TEXT, "Search for Classes").click()
-        # 选中Search by Term下拉框中的2023 Winter
+        # 选择查看的学期
         sel = select(driver.find.element(By.NAME, "p_term"))
         sel.select_by_value("202310")
         # sel.select_by_visible_text("2023 Winter")
         # sel.select_by_index("1")
         # 搜索
         driver.find.element(By.XPATH, "//input[@value='Submit']").click()
-        # 选中Subject下拉框中的Computer Science
+        # 选择查看的课程分类
         sel = select(driver.find.element(By.NAME, "sel_subj"))
         sel.select_by_value("CS")
         # sel.select_by_visible_text("Computer Science")
         # sel.select_by_index("17")
         # 搜索
         driver.find.element(By.XPATH, "//input[@value='Course Search']").click()
-        # 点击查看335 Computer Networks课程
+        # 选择查看的课程
         driver.find.element(By.NAME, "SUB_BTN").click()
         
      def test_3_add_and_delete_class(self):
@@ -87,9 +87,9 @@ class TestCase(unittest.TestCase):
         driver.implicitly_wait(10)
         # 加载网页
         driver.get("https://https://www.uregina.ca/")
-        # Link_text定位
+        # 选择当前学生界面
         driver.find.element(By.LINK_TEXT, "Current Students").click()
-        # Link_text定位
+        # 选择自助服务界面
         driver.find.element(By.LINK_TEXT, "UR Self-Service").click()
         # 输入用户ID和密码
         driver.find.element(By.NAME, "sid").send_keys("*********")
@@ -103,21 +103,21 @@ class TestCase(unittest.TestCase):
         driver.find.element(By.LINK_TEXT, "Registration").click()
         # 查询课程
         driver.find.element(By.LINK_TEXT, "Search for Classes").click()
-        # 选中Search by Term下拉框中的2023 Winter
+        # 选择查看的学期
         sel = select(driver.find.element(By.NAME, "p_term"))
         sel.select_by_value("202310")
         # sel.select_by_visible_text("2023 Winter")
         # sel.select_by_index("1")
         # 搜索
         driver.find.element(By.XPATH, "//input[@value='Submit']").click()
-        # 选中Subject下拉框中的Computer Science
+        # 选择查看的课程分类
         sel = select(driver.find.element(By.NAME, "sel_subj"))
         sel.select_by_value("CS")
         # sel.select_by_visible_text("Computer Science")
         # sel.select_by_index("17")
         # 搜索
         driver.find.element(By.XPATH, "//input[@value='Course Search']").click()
-        # 点击查看335 Computer Networks课程
+        # 选择查看的课程
         driver.find.element(By.NAME, "SUB_BTN").click()
         
         # 注册课程
@@ -152,7 +152,18 @@ class TestCase(unittest.TestCase):
         # 点击登录
         driver.find.element(By.XPATH, "//input[@value='Login']").click()
         
-        
+        # 进入学生界面
+        driver.find.element(By.LINK_TEXT, "Student").click()
+        # 进入学生记录界面
+        driver.find.element(By.LINK_TEXT, "Student Records").click()
+        # 查看分数
+        driver.find.element(By.LINK_TEXT, "Final Grades").click()
+        # 选择查看的学期
+        sel = select(driver.find.element(By.NAME, "term_in"))
+        sel.select_by_value("202210")
+        # 提交申请
+        driver.find.element(By.XPATH, "//input[@value='Submit']").click()
+        # 就可以查看到学期所有课程的分数
         
  if __name__ == '__main__':
      unittest.main()
