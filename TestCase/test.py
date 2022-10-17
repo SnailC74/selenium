1fb335146aeb78d646073bd4df4d6b58decab386
Hongyu Chen <60499437+SnailC74@users.noreply.github.com>
2022-10-17 10:43:18 +0800

    Update test.py

--- TestCase/test.py
+++ TestCase/test.py
@@ -2,15 +2,6 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-# ID定位
-//driver.find.element(By.ID, "kw").send_keys("University of Regina")
-# Name定位
-//driver.find.element(By.NAME, "wd").send_keys("University of Regina")
-# Link_text定位
-//driver.find.element(By.LINK_TEXT, "地图").click()
-# Partial_link定位
-//driver.find.element(By.PARTIAL_LINK, "图").click()
-
 class TestCase(unittest.TestCase):
     def test_1_login(self):
         """
@@ -26,11 +17,7 @@ class TestCase(unittest.TestCase):
         driver.find.element(By.LINK_TEXT, "Current Students").click()
         # 选择自助服务界面
         driver.find.element(By.LINK_TEXT, "UR Self-Service").click()
-        # 输入用户ID和密码
-        driver.find.element(By.NAME, "sid").send_keys("*********")
-        driver.find.element(By.NAME, "PIN").send_keys("********")
-        # 点击登录
-        driver.find.element(By.XPATH, "//input[@value='Login']").click()
+        lg = LoginPage()
         
     def test_2_search_class(self):
         """
