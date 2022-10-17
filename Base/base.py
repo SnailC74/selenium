2c70c0b43094b94f2dad2f9ed8bc9a94d808b6d3
Hongyu Chen <60499437+SnailC74@users.noreply.github.com>
2022-10-17 10:26:13 +0800

    Update base.py

--- Base/base.py
+++ Base/base.py
@@ -1,1 +1,12 @@
+class BasePage：
 
+    def __init__(self):
+        global driver
+        # 打开浏览器
+        self.driver = webdriver.chrome()
+        driver = self.driver
+        # 加载网页
+        self.driver.get("https://https://www.uregina.ca/") 
+        
+    def locator_element(self, loc):
+        return self.driver.find.element(loc)
