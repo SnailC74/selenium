e6711bec0f119f1f34123049be0d04882ccbb2d3
Hongyu Chen <60499437+SnailC74@users.noreply.github.com>
2022-10-18 11:17:32 +0800

    Update search_class.py

--- PageObject/search_class.py
+++ PageObject/search_class.py
@@ -4,8 +4,6 @@ from Base.base_page import BasePage
 class SearchClassPage(BasePage):
       
     # 页面元素
-    studentService_loc = (By.LINK_TEXT, "Student")
-    registration_loc = (By.LINK_TEXT, "Registration")
     searchClass_loc = (By.LINK_TEXT, "Search for Classes")
     p_term_loc = (By.NAME, "p_term")
     submit_loc = (By.XPATH, "//input[@value='Submit']")
@@ -15,8 +13,6 @@ class SearchClassPage(BasePage):
     
     # 页面动作
     def search_class(self):
-        self.click(SearchClassPage.studentService_loc)
-        self.click(SearchClassPage.registration_loc)
         self.click(SearchClassPage.searchClass_loc)
         self.select(SearchClassPage.p_term_loc, "202310")
         self.click(SearchClassPage.submit_loc)
