5d3bdb5ddb4a12333ed384010de09030dc3b1669
Hongyu Chen <60499437+SnailC74@users.noreply.github.com>
2022-10-18 15:30:44 +0800

    Update login_page.py

--- PageObject/login_page.py
+++ PageObject/login_page.py
@@ -7,9 +7,14 @@ class LoginPage(BasePage):
     userId_loc = (By.NAME, "sid")
     password_loc = (By.NAME, "PIN")
     login_loc = (By.XPATH, "//input[@value='Login']")
+    exit_loc = (By.LINK_TEXT, "EXIT")
     
     # 页面动作
     def login_service(self, userId = "*********", password = "********"):
         self.send_keys(LoginPage.userId_loc, userId)
         self.send_keys(LoginPage.password_loc, password)
         self.click(loginPage.login_loc)
+    
+    # 断言
+    def get_except_result(self):
+        return self.get_value(LoginPage.exit_loc)
