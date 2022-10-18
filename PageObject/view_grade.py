6779fc2428bcbac65abe30890b27f2ee5c4541e2
Hongyu Chen <60499437+SnailC74@users.noreply.github.com>
2022-10-18 11:23:17 +0800

    Update view_grade.py

--- PageObject/view_grade.py
+++ PageObject/view_grade.py
@@ -4,14 +4,12 @@ from Base.base_page import BasePage
 class ViewGradePage(BasePage):
       
     # 页面元素
-    studentRecord_loc = (By.LINK_TEXT, "Student Records")
     finalGrade_loc = (By.LINK_TEXT, "Final Grades")
     select_term_loc = (By.NAME, "term_in")
     submit_loc = (By.XPATH, "//input[@value='Submit']")
     
     # 页面动作
     def view_grade(self):
-        self.click(ViewGradePage.studentRecord_loc)
         self.click(ViewGradePage.finalGrade_loc)
         self.select(ViewGradePage.select_term_loc, "202210")
         self.click(ViewGradePage.submit_loc)
